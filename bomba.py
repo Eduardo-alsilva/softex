070cdbfbd25af46fd010833665bb51e70892e3b0
Eduardo-alsilva <114708309+Eduardo-alsilva@users.noreply.github.com>
2022-10-04 08:27:45 -0300

    Add files via upload

---
+++ bomba.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+print("Bomb has been planted..")
+print("Pi...pi..")
+
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(1)
+
+print("Bummm")
+print("Terrorist Win!!")
