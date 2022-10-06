ecd6aea031c997f02c4b3cf07846b4f29f77f9da
Eduardo-alsilva <114708309+Eduardo-alsilva@users.noreply.github.com>
2022-10-06 13:07:04 -0300

    Add files via upload
    calculadora com função de loop e sair em python

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,28 @@
 
 def calculadora (numero1, numero2, operacao):
     if (operacao == 1):
-        resultado = float(print(numero1 + numero2))
+        resultado = (print(numero1 + numero2))
     elif(operacao == 2):
-       resultado = float(print (numero1 - numero2))
+       resultado = (print (numero1 - numero2))
     elif(operacao == 3):
-       resultado = float(print (numero1 * numero2))
+       resultado = (print (numero1 * numero2))
     elif(operacao == 4):
-       resultado = float(print (numero1 / numero2))
-    elif(operacao >4):
-     print("Operação invalida")
+       resultado = (print (numero1 / numero2))
+   
+operacao =1
 
-print("soma-1\n" ,"subtração-2\n" , "multiplicação-3\n" , "divisão-4\n" )
-operacao= int(input("digite a operação: "))
-numero1= float(input("Digite um numero: "))
-numero2= float(input("Digite um numero: "))
-calculadora (numero1, numero2, operacao)
+while operacao:
+   print("soma-1\n" ,"subtração-2\n" , "multiplicação-3\n" , "divisão-4\n" , "sair-0\n" )
+
+   operacao = int(input('\nOperador: '))
+   if (operacao==0):
+      print("calc finalizada")
+      exit()
+   elif (operacao>=1 or operacao<=4):
+      numero1= float(input("Digite um numero: "))
+      numero2= float(input("Digite um numero: "))
+      calculadora (numero1, numero2, operacao)
+   else:
+      print("invalido")
+
 
