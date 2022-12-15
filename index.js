83e3f673b376337f56a62f9e938d3e493470176c
Eduardo-alsilva <114708309+Eduardo-alsilva@users.noreply.github.com>
2022-12-15 05:34:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPEhtml >
+< html  lang =" pt-bt " >
+< cabeça >
+    < meta  charset =" UTF-8 " >
+    < meta  name =" janela de visualização " conteúdo =" largura=largura do dispositivo, escala inicial=1,0 " >
+    < link  rel =" folha de estilo " href =" assets/css/main.css " >
+    < título > Token </ título >
+    < link  rel =" preconnect " href =" https://fonts.googleapis.com " >
+    < link  rel =" preconnect " href =" https://fonts.gstatic.com " crossorigin >
+</ cabeça >
+< corpo >
+    < div  classe =" recipiente " >
+        < p > Qual o tipo de atendimento desejado?: </ p >
+            < div  class =" botoes " >
+                < a  href =" atendimento_normal.html " > Normal </ a >
+            </ div >
+            < div  class =" botoes " >
+                < a  href =" atendimento_normal.html " > Preferencial </ a >
+            </ div >
+    </ div >
+</ corpo >
+</ html >
